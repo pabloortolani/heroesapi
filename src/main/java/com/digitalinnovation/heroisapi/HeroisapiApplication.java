984ddf8ad994b08223336f782e414df01094e7af
Pablo Ortolani <pabloortolani@gmail.com>
2021-04-19 18:57:18 -0300

    first commit

---
+++ src/main/java/com/digitalinnovation/heroisapi/HeroisapiApplication.java
@@ -0,0 +1,15 @@
+package com.digitalinnovation.heroisapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroisapiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HeroisapiApplication.class, args);
+		System.out.println("Super Poderes com webflux");
+	}
+
+}
---
+++ src/main/java/com/digitalinnovation/heroisapi/repository/HeroesRepository.java
@@ -0,0 +1,9 @@
+package com.digitalinnovation.heroisapi.repository;
+
+import com.digitalinnovation.heroisapi.document.Heroes;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan
+public interface HeroesRepository extends CrudRepository<Heroes, String>{
+}
